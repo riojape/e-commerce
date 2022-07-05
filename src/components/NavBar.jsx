5e7070690f273d5d0e92cd033fff4a2128f5b48d
riojape <edinrioas@gmail.com>
2022-07-05 17:23:24 -0500

    Header

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+        <ul>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Tienda</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
